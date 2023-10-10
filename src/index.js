1bdac3390464b2f8f4eace5a8031634f9f0fac1e
Bianca10 <bianca.maris12@gmail.com>
2023-10-10 15:00:25 +0300

    primul commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import 'stream-browserify';
+import 'buffer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
